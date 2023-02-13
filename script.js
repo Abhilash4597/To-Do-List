eb1f083f4ef12595cdb10992cc1783b71164afc9
Abhilash4597 <abhilash4597@gmail.com>
2023-02-14 01:39:37 +0530

    setting and getting in localstorage done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const input = document.querySelector('#input');
+const btn = document.querySelector('#button-addon2');
+const mainUl = document.querySelector('.list-group');
+
+const editBtn = document.querySelector('.edit');
+const deleteBtn = document.querySelector('.delete');
+const alarmBtn = document.querySelector('.alarm');
+let inputValue, taskArr;
+
+btn.addEventListener('click', function () {
+  console.log('clicked')
+  inputValue = input.value;
+  let allTask = localStorage.getItem('task');
+  if (allTask == null) {
+    taskArr = []; 
+  } else {
+    taskArr = JSON.parse(allTask);
+  }
+  taskArr.push(inputValue);
+  localStorage.setItem('task', JSON.stringify(taskArr));
+})
