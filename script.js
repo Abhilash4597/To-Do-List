f116dcb8791c73b7717267378ec9bd81c84f201c
Abhilash4597 <abhilash4597@gmail.com>
2023-02-14 02:07:58 +0530

    adding fn done

--- script.js
+++ script.js
@@ -7,17 +7,52 @@ const mainUl = document.querySelector('.list-group');
 const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
 const alarmBtn = document.querySelector('.alarm');
-let inputValue, taskArr;
+let taskArr, inputValue, allTask, html;
 
 btn.addEventListener('click', function () {
-  console.log('clicked')
   inputValue = input.value;
-  let allTask = localStorage.getItem('task');
+  allTask = localStorage.getItem('task');
   if (allTask == null) {
-    taskArr = []; 
+    taskArr = [];
   } else {
     taskArr = JSON.parse(allTask);
   }
   taskArr.push(inputValue);
   localStorage.setItem('task', JSON.stringify(taskArr));
-})
+  // displayTask();
+  taskArr.forEach(element => {
+    html += `<li class="list-group-item list-group-item-dark line-height"><input class="form-check-input me-1" type="checkbox"
+        value="" id="firstCheckbox">
+      <label class="form-check-label" for="firstCheckbox">${element}</label>
+      <div>
+        <button class="edit">Edit<i class="fa-solid fa-keyboard"></i></button>
+        <button class="delete">Delete<i class="fa-regular fa-trash-can"></i></button>
+        <button class="alarm">Alarm<i class="fa-solid fa-bell"></i></button>
+      </div>
+    </li>`;
+  });
+  mainUl.innerHTML = html;
+});
+
+// function displayTask() {
+//   inputValue = input.value;
+//   allTask = localStorage.getItem('task');
+//   if (allTask == null) {
+//     taskArr = [];
+//   } else {
+//     taskArr = JSON.parse(allTask);
+//   }
+//   taskArr.forEach(element => {
+//     html += `<li class="list-group-item list-group-item-dark line-height"><input class="form-check-input me-1" type="checkbox"
+//         value="" id="firstCheckbox">
+//       <label class="form-check-label" for="firstCheckbox">${element}</label>
+//       <div>
+//         <button class="edit">Edit<i class="fa-solid fa-keyboard"></i></button>
+//         <button class="delete">Delete<i class="fa-regular fa-trash-can"></i></button>
+//         <button class="alarm">Alarm<i class="fa-solid fa-bell"></i></button>
+//       </div>
+//     </li>`;
+//   });
+//   mainUl.innerHTML = html;
+// }
+// displayTask();
