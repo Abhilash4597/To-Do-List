bf36b42190dc5e1b181bc14d80bb25b6c9bca6e7
Abhilash4597 <abhilash4597@gmail.com>
2023-02-14 14:00:10 +0530

    updated

--- script.js
+++ script.js
@@ -2,14 +2,44 @@
 
 const input = document.querySelector('#input');
 const btn = document.querySelector('#button-addon2');
-const mainUl = document.querySelector('.list-group');
+const mainUl = document.querySelector('.allList');
 
 const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
 const alarmBtn = document.querySelector('.alarm');
 let taskArr, inputValue, allTask, html;
 
-btn.addEventListener('click', function () {
+
+//   function () {
+//   inputValue = input.value;
+//   allTask = localStorage.getItem('task');
+//   if (allTask == null) {
+//     taskArr = [];
+//   } else {
+//     taskArr = JSON.parse(allTask);
+//   }
+//   taskArr.push(inputValue);
+//   localStorage.setItem('task', JSON.stringify(taskArr));
+//   // displayTask();
+//   taskArr.forEach(element => {
+//     html += `<ul class="list-group margin width border">
+//     <li class="list-group-item list-group-item-dark line-height overFlow"><input class="form-check-input me-1" type="checkbox"
+//         value="" id="firstCheckbox">
+//       <label class="form-check-label" for="firstCheckbox">${element}</label>
+//     </li>
+//     <div class="margin">
+//         <button class="edit">Edit<i class="fa-solid fa-keyboard"></i></button>
+//         <button class="delete">Delete<i class="fa-regular fa-trash-can"></i></button>
+//         <button class="alarm">Alarm<i class="fa-solid fa-bell"></i></button>
+//       </div>
+
+//   </ul>
+// `;
+//   mainUl.innerHTML = html;
+//   });
+// });
+
+function displayTask() {
   inputValue = input.value;
   allTask = localStorage.getItem('task');
   if (allTask == null) {
@@ -20,39 +50,22 @@ btn.addEventListener('click', function () {
   taskArr.push(inputValue);
   localStorage.setItem('task', JSON.stringify(taskArr));
   // displayTask();
-  taskArr.forEach(element => {
-    html += `<li class="list-group-item list-group-item-dark line-height"><input class="form-check-input me-1" type="checkbox"
+  taskArr.map(element => {
+    html += `
+    <li class="list-group-item list-group-item-dark line-height overFlow"><input class="form-check-input me-1" type="checkbox"
         value="" id="firstCheckbox">
       <label class="form-check-label" for="firstCheckbox">${element}</label>
-      <div>
+    </li>
+    <div class="margin">
         <button class="edit">Edit<i class="fa-solid fa-keyboard"></i></button>
         <button class="delete">Delete<i class="fa-regular fa-trash-can"></i></button>
         <button class="alarm">Alarm<i class="fa-solid fa-bell"></i></button>
       </div>
-    </li>`;
-  });
-  mainUl.innerHTML = html;
-});
 
-// function displayTask() {
-//   inputValue = input.value;
-//   allTask = localStorage.getItem('task');
-//   if (allTask == null) {
-//     taskArr = [];
-//   } else {
-//     taskArr = JSON.parse(allTask);
-//   }
-//   taskArr.forEach(element => {
-//     html += `<li class="list-group-item list-group-item-dark line-height"><input class="form-check-input me-1" type="checkbox"
-//         value="" id="firstCheckbox">
-//       <label class="form-check-label" for="firstCheckbox">${element}</label>
-//       <div>
-//         <button class="edit">Edit<i class="fa-solid fa-keyboard"></i></button>
-//         <button class="delete">Delete<i class="fa-regular fa-trash-can"></i></button>
-//         <button class="alarm">Alarm<i class="fa-solid fa-bell"></i></button>
-//       </div>
-//     </li>`;
-//   });
-//   mainUl.innerHTML = html;
-// }
+  
+`;
+});
+mainUl.innerHTML = html;
+}
+btn.addEventListener('click', displayTask);
 // displayTask();
